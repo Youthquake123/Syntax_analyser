aa556dd5c92cf6c57b52dd94764ac6eac284cf45
AX ZHANG <136069021+Youthquake123@users.noreply.github.com>
2023-12-11 14:07:42 +0800

    Update Compile.java

--- Compile.java
+++ Compile.java
@@ -1,20 +1,11 @@
-
-/**
- *
- * Main Driver program for 312 exercise.
- * 
- * This class has been provided to students
- *
- * @Author: Roger Garside, John Mariani, John Vidler, Paul Rayson
- *
- *
- **/
-
 import java.io.* ;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Compile {
 
 	public static String fileName;
+	private static final String INDENT = "    "; // 4 spaces for indentation
 
 	/**
 	 *
@@ -22,15 +13,21 @@ public class Compile {
 	 *
 	 **/
 
-	private void go() throws IOException {
-		String prefix = "Programs Folder" + File.separator + "program";
-		int fileNumber = -1;
-		int exitFlag = 0;
+	private void go(String outputFile) throws IOException {
+		System.out.println("Please choose the input file, 1: Right test case, 2: Wrong input test");
+		Scanner input =new Scanner(System.in);
+		int choose = input.nextInt();
 		PrintStream out = null;
-		String outputFile = new String( "output.txt" );
 		boolean goon = true;
+		//outputFile = new String( "output1.txt" );
 
 		try {
+			if(choose==1){
+				fileName = "test.txt";
+			}
+			if(choose==2){
+				fileName = "wrongTest.txt";
+			}
 			out = new PrintStream( new FileOutputStream(outputFile) );
 		} catch( Exception e ) {
 			System.out.println("unable to open output file "+e);
@@ -38,31 +35,71 @@ public class Compile {
 		}
 
 		while( goon ) {
-//			fileNumber++ ;
-//			fileName = prefix + fileNumber;
-			fileName = "test_2.txt";
 			goon = ((new File(fileName)).exists());
 			if( goon ) {
 				System.out.println();
 
 				SyntaxAnalyser syn = new SyntaxAnalyser(fileName) ;
 				System.setOut(out);
-				System.out.println( "312START" );
-				System.out.println( "312FILE " + fileName );
+				System.out.println( "RDPSTART" );
+				System.out.println( "RDPFILE " + fileName );
 				syn.parse( out ) ;
+				System.out.println() ;
+				System.out.println("RDPFINISH") ;
+
 				break;
 			} else System.out.println(fileName+" does not exist");
 		}
 
-		System.out.println() ;
-		System.out.println("312FINISH") ;
 		out.flush();out.close();
-		System.exit(exitFlag) ;
 	} // end of main method
+	public static void convertToTree(String filePath, String outputFilePath) {
+		File inputFile = new File(filePath);
+		File outputFile = new File(outputFilePath);
+
+		Stack<String> stack = new Stack<>();
+		int currentIndentation = 0;
+
+		try (BufferedReader br = new BufferedReader(new FileReader(inputFile));
+			 PrintWriter out = new PrintWriter(new FileWriter(outputFile))) {
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (line.contains("BEGIN")) {
+					printWithIndentation(out, currentIndentation, line);
+					// Increase indentation and push to stack
+					stack.push(line);
+					currentIndentation++;
+				} else if (line.contains("END")) {
+					// Pop from stack and decrease indentation
+					if (!stack.isEmpty()) stack.pop();
+					currentIndentation--;
+					printWithIndentation(out, currentIndentation, line);
+				} else {
+					// Print tokens and other lines with current indentation
+					printWithIndentation(out, currentIndentation, line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void printWithIndentation(PrintWriter out, int indentationLevel, String content) {
+		for (int i = 0; i < indentationLevel; i++) {
+			out.print(INDENT);
+		}
+		out.println(content.trim());
+	}
 
 	public static void main(String args[]) throws IOException {
 		Compile c = new Compile();
-		c.go();
-	};
 
+		String outputFile = new String( "output1.txt" );
+		c.go(outputFile);
+		String outputFilePath = "output.txt";
+		convertToTree(outputFile, outputFilePath);
+		File fileToDelete = new File(outputFile);
+		fileToDelete.delete();
+	};
 } // end of class Compile
